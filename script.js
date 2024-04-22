b142223ac2c2f2c818370dbb46b25b38e54c43d5
Winston Nichols <157043193+winstonnichols@users.noreply.github.com>
2024-04-22 03:10:54 +0000

    Project 3 v3 completed

--- script.js
+++ script.js
@@ -7,38 +7,40 @@
 var gameState = "splash";
 var player1;
 var timer;
+var testBox;
+var dropTimer;
+var presents = new Array(0); // empty array
+
+
 
 
 function setup() {
-
-
   createCanvas(600, 400);
   player1 = new Player(width/2, height * 4/5);
-  player1.x = mouseX;
-  timer = new Timer(5000)
+  testBox = new Box(width/2, height/3);
+  timer = new Timer(10000); // 10 second timer
+  dropTimer = new Timer(1000); // 1 second timer for dropping presents
 }
+
+
 
 
 function draw() {
   background(200);
-  /* un-comment each line to see it work */
-  //splash(); // call the splash screen function (below)
-  //play(); // call the play screen function (below)
-  //gameOver(); // call the gameOver screen function (below)
-  switch(gameState){
-    case "splash" :
+  switch(gameState) {
+    case "splash":
       splash();
       break;
-    case "play" :
+    case "play":
       play();
       break;
     case "gameOver":
       gameOver();
       break;
-    default :
-      console.log("no switch found")
-   }
+    default:
+      console.log("no switch found");
   }
+}
 
 
 
@@ -46,31 +48,53 @@ function draw() {
 
 
 function splash() {
-  // this is what you would see when the game starts
   background(200);
   textAlign(CENTER);
   textSize(16);
   text("Let's Play a Game!", width / 2, height / 2);
   textSize(12);
   text("(click the mouse to continue)", width / 2, height / 2 - 20);
+  testBox.display();
+  testBox.spin();
+  //testBox.move();
 }
 
 
 function play() {
-  // this is what you see when the game is running 
   background(0, 200, 0);
-  fill(0, 0, 200)
+  fill(0, 0, 200);
   textAlign(CENTER);
   textSize(16);
-  // text("This is where the Game happens", width / 2, height / 2);
-  // player1.x = mouseX;
   player1.display();
-
+  
   if(timer.isFinished()){
     gameState = "gameOver";
   }
+  if(dropTimer.isFinished()){
+    let p = new Box(random(width), -40);
+    presents.push(p);
+    dropTimer.start();
+  }
+ 
+  for(let i = 0; i < presents.length; i++){
+    presents[i].display();
+    presents[i].move();
+    presents[i].spin();
+    
+    if(presents[i].y > height){
+      presents.splice(i, 1); // remove from array
+    }
+    
+    let d = dist(presents[i].x, presents[i].y, player1.x, player1.y);
+    if(d < 50){
+      presents.splice(i, 1);
+    }
+
+  } // end of for() loop
+  
   text("elapsed time: " + timer.elapsedTime, width/2, height - 20);
 }
+
 
 function gameOver() {
   // this is what you see when the game ends
@@ -87,6 +111,7 @@ function mousePressed() {
   if (gameState == "splash") {
     gameState = "play";
     timer.start();
+    dropTimer.start();
   } else if (gameState == "play") {
   //  gameState = "gameOver"; // Corrected assignment
   } else if (gameState == "gameOver") {
